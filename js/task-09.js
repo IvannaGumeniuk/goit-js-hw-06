4662701155192970da5cc4078da5d3f4e2bf0823
IvannaGumeniuk <vany93ok@gmail.com>
2022-05-15 23:31:39 +0300

    Завдання 9 - done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const body = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const actualColor = document.querySelector('.color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+changeColorBtn.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  actualColor.innerHTML = color;
+  body.style.backgroundColor = color;
+});
