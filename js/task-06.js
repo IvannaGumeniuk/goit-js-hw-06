572d69ef5330bd70b9ff45de4a62441082605b8f
IvannaGumeniuk <vany93ok@gmail.com>
2022-05-18 22:54:18 +0300

    Завдання 6 - fixed

--- js/task-06.js
+++ js/task-06.js
@@ -2,13 +2,16 @@ const textInput = document.querySelector('#validation-input');
 textInput.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-  if ((event.currentTarget.value.length) > textInput.dataset.length) {
-    textInput.classList.add("invalid");
+
+  if ((event.currentTarget.value.length) === Number(textInput.dataset.length)) {
+    textInput.classList.remove("invalid");
+    textInput.classList.add("valid");
   }
 
   else {
-    textInput.classList.add("valid");
-  }
+    textInput.classList.remove("valid");
+    textInput.classList.add("invalid"); 
 }
+};
 
 
